08ee1a0fab1e5147c3a470474a1041e09fc56bd9
BoxMaslo <maclerkov@gmail.com>
2016-10-01 21:58:08 +0300

    Первая работа python

---
+++ python/task1.py
@@ -0,0 +1,9 @@
+﻿print u'Введите строку для реверса:'
+st = list(raw_input())
+st.reverse()
+print u'Результат: \n' + "".join(st)
+
+print u'Введите строку для сортировки:'
+st_int = list(raw_input())
+st_int.sort()
+print u'Результат: \n' + ''.join(str(a) for a in st_int)
